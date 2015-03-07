9096b16167fd3a8f2196f9296494555ae3ad0499
Inderpal Singh <inderps@thoughtworks.com>
2015-03-08 01:14:31 +0530

    added login_check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :require_login
+
+  def require_login
+    @user = User.find_by_id(session[:user_id])
+    unless @user
+      redirect_to login_url
+    end
+  end
 end
